38020ef86dbf0c79e27a4f655073623a58875fac
Toad Miranda <tmiranda@pr-a5.kzn.21-school.ru>
2022-05-15 15:25:33 +0300

    draft

--- vector.hpp
+++ vector.hpp
@@ -15,10 +15,25 @@ namespace ft {
                 typedef typename allocator_type::const_reference const_reference;
                 typedef typename allocator_type::pointer pointer;
                 typedef typename allocator_type::const_pointer const_pointer;
-
-
-                explicit vector(const allocator_type& alloc = allocator_type());
-
+                typedef typename RandomAccessIterator iterator;
+                //typedef typename RandomAccessIterator const_iterator;
+                //typedef typename ReverseIterator<iterator> reverse_iterator;
+//                typedef typename ReverseIterator<const_iterator> const_reverse_iterator;
+//                typedef typename iterator_traits<iterator>::difference_type difference_type;
+                typedef typename size_t size_type;
+//                explicit vector(const allocator_type& alloc = allocator_type()) : _array(nullptr), _capacity(0), _size(0), _alloc(alloc) {};
+//                explicit vector (size_type n, const value_type& val = value_type(),
+//                                 const allocator_type& alloc = allocator_type()) : _capacity(n), _size(n), _alloc(alloc) {
+//                    _array = _alloc.allocate(n);
+//                    for(size_t i = 0; i < n; i++) {
+//                        _array = _alloc.construct();
+//                    }
+                };
+            private:
+                value_type* _array;
+                size_type _capacity;
+                size_type _size;
+                allocator_type _alloc;
             };
 }
 
